1eca9bd7d5651a8ebed086ab1f5e6e10df541099
natalliakarnilava <bonachek@gmail.com>
2019-09-27 11:51:28 +0300

    daoTester added

---
+++ src/test/java/AccountDAOTest.java
@@ -0,0 +1,13 @@
+import dto.Client;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDAOTest {
+
+    @Test
+    void create() {
+        int i=1;
+        assertEquals(1,i);
+    }
+}
